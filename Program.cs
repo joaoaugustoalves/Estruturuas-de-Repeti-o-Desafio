852444f02645815c23e0e39bc7d03f2dc206b402
Joao Augusto <frajolacommel@gmail.com>
2020-11-24 19:13:59 -0300

    concluido

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int anterior = 0, atual = 1, soma;
+
+           Console.WriteLine();
+
+           Console.Write("0 1 ");
+
+           
+        do {
+            soma = anterior + atual;
+
+            anterior = atual;
+
+            atual = soma; 
+            
+            Console.Write($"{soma} " );
+            
+            }
+                  
+            while (atual < 500);      
+
+            Console.WriteLine(".");
+        }
+    }
+}
